672ae25154a6562a052b658acaf802cf56d190a1
Natalya Sorokina <natali83911@gmail.com>
2025-04-10 12:33:18 +0300

    modified file

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
 import json
 import os
+from typing import Any, Dict, List
 
-def load_transactions_from_json(file_path):
-    """ Загружает список финансовых транзакций из JSON-файла.
-    Возвращает пустой список, если файл не найден, пуст или содержит не список. """
+
+def load_transactions_from_json(file_path: str) -> List[Dict[str, Any]]:
+    """Загружает список финансовых транзакций из JSON-файла.
+    Возвращает пустой список, если файл не найден, пуст или содержит не список."""
     try:
-        with open(file_path, 'r', encoding='utf-8') as f:
+        with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
             if isinstance(data, list):
                 return data
@@ -22,7 +24,7 @@ def load_transactions_from_json(file_path):
 # Пример использования (для проверки)
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-if __name__ == '__main__':
-    file_path = os.path.join(PROJECT_ROOT, 'data', 'operations.json')
+if __name__ == "__main__":
+    file_path = os.path.join(PROJECT_ROOT, "data", "operations.json")
     transactions = load_transactions_from_json(file_path)
     print(transactions)
