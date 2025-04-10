e0831cfef33dfa08128d6a508e186b6b8c3f79fe
Natalya Sorokina <natali83911@gmail.com>
2025-04-10 14:32:31 +0300

    added file external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,42 @@
+import os
+from typing import Dict, Any
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+
+def convert_to_rub(transaction: Dict) -> Any:
+    """Конвертирует сумму транзакции в рубли.
+    Для USD/EUR использует текущий курс через API."""
+    operation_amount = transaction.get("operationAmount", {})
+    amount = operation_amount.get("amount", 0)
+    currency = operation_amount.get("currency", {}).get("code", "RUB").upper()
+
+    if amount and currency:
+        try:
+            amount = float(amount)
+        except ValueError:
+            print(f"Ошибка: Некорректный формат суммы: {amount}")
+            return None
+
+    if currency == "RUB":
+        return amount
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+    headers = {"apikey": API_KEY}
+
+    try:
+        response = requests.get(url, headers=headers)
+        status_code = response.status_code
+
+        if status_code == 200:
+            return response.json()["result"]
+        else:
+            print(f"Запрос не был успешным. Возможная причина: {response.reason}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Произошла ошибка {str(e)}")
+        return None
